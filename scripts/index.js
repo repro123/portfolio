eb457f2a4dbd8603eed44b29994413b194b46e76
Bello Ibrahim <ibrahimopeyemibello@gmail.com>
2025-04-23 01:01:03 +0100

    functionality updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,31 @@
 "use strict";
+/////////////////////////////////////////////////////////////
+// selecting all the elements that will be used in the script
 
+// mobile nav tggle button
 const navToggleBtn = document.getElementById("navToggle");
+
+// selecting all the buttons in the FAQ section
 const accordionBtn = document.querySelectorAll(".accordion__button");
 
+// selecting the back to top button
+const backToTopBtn = document.getElementById("backToTop");
+const scrollThreshold = 500; // 500px
+
+// selecting the form elements
+const form = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const nameError = document.getElementById("nameError");
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+const messageInput = document.getElementById("message");
+const messageError = document.getElementById("messageError");
+const submitBtn = document.getElementById("submitBtn");
+const successAlert = document.getElementById("successAlert");
+const failAlert = document.getElementById("failAlert");
+
+///////////////////////////////////////////////////////////////////
+// mobile nav toggle button - add event listener to the button to toggle the aria-expanded attribute
 navToggleBtn.addEventListener("click", function () {
   const isAriaExpanded = navToggleBtn.getAttribute("aria-expanded") === "true"; // isAriaExpanded is false
   navToggleBtn.setAttribute("aria-expanded", !isAriaExpanded);
@@ -29,8 +52,6 @@ for (let i = 0; i < accordionBtn.length; i++) {
 
 //////////////////////////////////////////////////////////////////
 // back to top section
-const backToTopBtn = document.getElementById("backToTop");
-const scrollThreshold = 500; // 500px
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > scrollThreshold) {
@@ -51,6 +72,147 @@ backToTopBtn.addEventListener("click", function () {
 });
 
 //////////////////////////////////////////////////////////////////
+// footer copyright year - get the copyright year element in the footer and set its text content to the current year
+
 const copyrightYear = document.getElementById("copyrightYear");
-const currentYear = new Date().getFullYear(); // get the current year
-copyrightYear.textContent = currentYear; // set the text content of the copyright year to the current year
+const currentYear = new Date().getFullYear();
+copyrightYear.textContent = currentYear;
+
+//////////////////////////////////////////////////////////////////
+// time stamp for the form submission
+
+function setCurrentDateTime() {
+  const now = new Date();
+  const options = {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false, // Use 24-hour format
+  };
+
+  const formattedDate = now
+    .toLocaleDateString("en-US", options)
+    .replace(/,/g, "") // Remove commas
+    .replace(/ ([AP]M)/, ""); // Remove AM/PM if needed
+
+  document.getElementById("timestamp").value = formattedDate;
+}
+
+///////////////////////////////////////////////////////////
+// form and input validation
+
+function validateName() {
+  let isValid = true;
+  const nameValue = nameInput.value.trim();
+  nameInput.removeAttribute("aria-invalid");
+  nameInput.removeAttribute("aria-describedby");
+  nameInput.removeAttribute("data-invalid");
+  nameError.textContent = "";
+
+  if (nameValue === "") {
+    nameError.textContent = "Can not be empty.";
+    nameInput.setAttribute("aria-invalid", "true");
+    nameInput.setAttribute("aria-describedby", "nameError");
+    nameInput.setAttribute("data-invalid", "true");
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+function validateEmail() {
+  let isValid = true;
+  const emailValue = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  const isValidEmail = emailRegex.test(emailValue);
+
+  emailInput.removeAttribute("aria-invalid");
+  emailInput.removeAttribute("aria-describedby");
+  emailInput.removeAttribute("data-invalid");
+  emailError.textContent = "";
+
+  if (emailValue === "") {
+    emailError.textContent = "Can not be empty.";
+    emailInput.setAttribute("aria-invalid", "true");
+    emailInput.setAttribute("aria-describedby", "emailError");
+    emailInput.setAttribute("data-invalid", "true");
+    isValid = false;
+  } else if (!isValidEmail) {
+    emailError.textContent = "Enter a valid email address.";
+    emailInput.setAttribute("aria-invalid", "true");
+    emailInput.setAttribute("aria-describedby", "emailError");
+    emailInput.setAttribute("data-invalid", "true");
+    isValid = false;
+  }
+  return isValid;
+}
+
+function validateMessage() {
+  let isValid = true;
+  const messageValue = messageInput.value.trim();
+  messageInput.removeAttribute("aria-invalid");
+  messageInput.removeAttribute("aria-describedby");
+  messageInput.removeAttribute("data-invalid");
+  messageError.textContent = "";
+
+  if (messageValue === "") {
+    messageError.textContent = "Cannot be empty.";
+    messageInput.setAttribute("aria-invalid", "true");
+    messageInput.setAttribute("aria-describedby", "messageError");
+    messageInput.setAttribute("data-invalid", "true");
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+nameInput.addEventListener("input", validateName);
+emailInput.addEventListener("input", validateEmail);
+messageInput.addEventListener("input", validateMessage);
+
+function validateForm() {
+  const isNameValid = validateName();
+  const isEmailValid = validateEmail();
+  const isMessageValid = validateMessage();
+
+  return isNameValid && isEmailValid && isMessageValid;
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent the default form submission
+  const isFormValid = validateForm(); // Validate the form
+
+  if (!isFormValid) return; // If the form is not valid, do not proceed
+
+  try {
+    setCurrentDateTime(); // Set the current date and time in the hidden input field
+    console.log("Form submitted successfully!");
+    await emailjs.sendForm("service_lpg9cnk", "template_0vlauoc", event.target); // Send the form data using EmailJS
+
+    await emailjs.send("service_lpg9cnk", "template_wfp4c2g", {
+      name: nameInput.value,
+      email: emailInput.value,
+      message: messageInput.value,
+      time: document.getElementById("timestamp").value,
+    });
+
+    console.log("Both emails sent successfully!");
+    form.reset(); // Reset the form after successful submission
+    successAlert.classList.remove("hidden"); // Show the success alert
+    submitBtn.setAttribute("aria-disabled", "true"); // Disable the submit button
+    submitBtn.classList.add("opacity-50", "pointer-events-none"); // Disable pointer events and reduce opacity
+    setTimeout(() => {
+      successAlert.classList.add("hidden"); // Hide the success alert after 3 seconds
+      submitBtn.removeAttribute("aria-disabled"); // Enable the submit button
+      submitBtn.classList.remove("opacity-50", "pointer-events-none"); // Enable pointer events and reset opacity
+    }, 3000); // 3 seconds delay
+  } catch (error) {
+    console.log("FAILED...", error);
+    failAlert.classList.remove("hidden"); // Show the failure alert
+    setTimeout(() => {
+      failAlert.classList.add("hidden"); // Hide the failure alert after 3 seconds
+    }, 3000); // 3 seconds delay
+  }
+});
---
+++ scripts/init.js
@@ -0,0 +1,6 @@
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "r_Bgdog2gYWwRfhZe",
+  });
+})();
